afae8c675d9add2db77432ef095f6a6463e44a36
YUSUF <yusu@duck.com>
2023-09-20 02:37:18 +0300

    more menu: arrows direction

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -9,7 +9,7 @@ export default function Menu() {
 		<nav className="mt-0.5 mb-1">
 
 			{mainMenu.map((menu, index) => (
-				<NavLink to={menu.path} className="py-[3px] block group">
+				<NavLink key={index} to={menu.path} className="py-[3px] block group">
 					{({isActive}) => (
 						<div
 							className={classNames("p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]", {
