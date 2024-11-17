dc7894368beb06394d1fd9bda6b4c376208713c0
Tayfun Erbilen <tayfunerbilen@gmail.com>
2024-11-17 11:32:19 +0300

    12. ders kodlari

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -3,15 +3,18 @@ import Premium from "~/layouts/main/rightbar/premium";
 import Topics from "~/layouts/main/rightbar/topics";
 import WhoFollow from "~/layouts/main/rightbar/who-follow";
 import Footer from "~/layouts/main/rightbar/footer";
+import StickyBox from "react-sticky-box";
 
 export default function RightBar() {
 	return (
-		<aside className="w-[350px] mr-2.5">
-			<Search />
-			<Premium />
-			<Topics />
-			<WhoFollow />
-			<Footer />
-		</aside>
+		<StickyBox className="self-start" offsetBottom={80}>
+			<aside className="w-[350px] mr-2.5">
+				<Search/>
+				<Premium/>
+				<Topics/>
+				<WhoFollow/>
+				<Footer/>
+			</aside>
+		</StickyBox>
 	)
 }
